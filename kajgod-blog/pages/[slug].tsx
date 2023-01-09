2912f1d5b896371daf4b6c1acae6b4c2949d0069
kajgod <kajgod@gmail.com>
2023-01-09 20:53:13 +0100

    feat: adding more posts

--- kajgod-blog/pages/[slug].tsx
+++ kajgod-blog/pages/[slug].tsx
@@ -1,14 +1,34 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
 import blog from "../data/blog.json";
 
-export default function Blog() {
-  const router = useRouter();
-  const { slug } = router.query;
+// Generates a page for each blog post
+// Called on build time 1st
+export async function getStaticPaths() {
+  const paths = blog.posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+// Called on build time 2nd
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  return {
+    // Passed to the page component as props
+    props: { slug: params.slug },
+  };
+}
+
+// Exports a React component
+// Generates runtime HTML
+export default function Blog({ slug }: { slug: string }) {
   const post = Object.values(blog.posts).find((post) => post.slug === slug);
   if (!post) return <h1>404</h1>;
-  console.log(post);
   return (
     <>
       <Head>
