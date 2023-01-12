ae163ac91713e4a09b725bce08a3ac2564853165
jstaresincic <jurica.staresincic@minus5.hr>
2023-01-12 07:12:46 +0100

    feat: add open graph and twitter meta

---
+++ kajgod-blog/components/Meta.tsx
@@ -0,0 +1,57 @@
+interface IMeta {
+  title: string;
+  description: string;
+  slug: string;
+  timeString: string;
+}
+
+const Meta = ({ title, description, slug, timeString }: IMeta) => (
+  <>
+    <title>{title}</title>
+    <link rel="icon" href="/favicon.ico" />
+    <link
+      rel="preconnect"
+      href="https://fonts.googleapis.com"
+      crossOrigin="true"
+    />
+    <link rel="canonical" href={`https://kajgod.github.io/${slug}`} />
+    <meta name="description" content={description} />
+    <meta property="og:type" content={slug !== "" ? "article" : "website"} />
+    <meta
+      property="og:title"
+      content={title}
+    />
+    <meta
+      property="og:description"
+      content={description}
+    />
+    <meta property="og:site_name" content="Kajgod Blog" />
+    <meta
+      property="article:author"
+      content="https://www.facebook.com/jurica.staresincic"
+    />
+    <meta property="article:section" content="Technology" />
+    <meta
+      property="article:published_time"
+      content={timeString}    />
+    {/* <meta */}
+    {/*   property="og:image" */}
+    {/*   content="https://smashingmagazine.com/images/smashing-homepage.png" */}
+    {/* /> */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta
+      name="twitter:description"
+      content={description}
+    />
+    <meta
+      name="twitter:title"
+      content={title}
+    />
+    {/* <meta */}
+    {/*   name="twitter:image" */}
+    {/*   content="https://smashingmagazine.com/images/smashing-homepage.png" */}
+    {/* /> */}
+  </>
+);
+
+export default Meta;
