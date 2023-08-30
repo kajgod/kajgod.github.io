921723a3c21e0181e54c7459b11301e2689653ed
kajgod <kajgod@gmail.com>
2023-08-30 14:02:07 +0200

    chore: add readme, favicon, update stuff

--- kajgod-blog/README.md
+++ kajgod-blog/README.md
@@ -1,1 +1,44 @@
-# MojStrip
+# Kajgod tech blog
+
+A blog about tech & tech philosophy
+
+## Installation
+
+Requires Node.js version 20+
+
+```bash
+  cd kajgod-blog
+  npm install
+```
+
+### Start dev
+
+```
+  npm run dev
+```
+
+### First build
+
+There aren't old files for removal
+
+```
+   npm run newbuild
+```
+
+### Normal build
+
+```
+  npm run build
+```
+
+### Build with bundle analyzer
+
+```
+  ANALYZE=true npm run build
+```
+
+## Tech Stack
+
+**Client:** Next.js, React
+
+**Build tools:** Node, (Go)
--- kajgod-blog/components/Meta.tsx
+++ kajgod-blog/components/Meta.tsx
@@ -12,41 +12,27 @@ const Meta = ({ title, description, slug, timeString }: IMeta) => (
     <link
       rel="preconnect"
       href="https://fonts.googleapis.com"
-      crossOrigin="true"
+      crossOrigin="anonymous"
     />
     <link rel="canonical" href={`https://kajgod.github.io/${slug}`} />
     <meta name="description" content={description} />
     <meta property="og:type" content={slug !== "" ? "article" : "website"} />
-    <meta
-      property="og:title"
-      content={title}
-    />
-    <meta
-      property="og:description"
-      content={description}
-    />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
     <meta property="og:site_name" content="Kajgod Blog" />
     <meta
       property="article:author"
       content="https://www.facebook.com/jurica.staresincic"
     />
     <meta property="article:section" content="Technology" />
-    <meta
-      property="article:published_time"
-      content={timeString}    />
+    <meta property="article:published_time" content={timeString} />
     {/* <meta */}
     {/*   property="og:image" */}
     {/*   content="https://smashingmagazine.com/images/smashing-homepage.png" */}
     {/* /> */}
     <meta name="twitter:card" content="summary_large_image" />
-    <meta
-      name="twitter:description"
-      content={description}
-    />
-    <meta
-      name="twitter:title"
-      content={title}
-    />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:title" content={title} />
     {/* <meta */}
     {/*   name="twitter:image" */}
     {/*   content="https://smashingmagazine.com/images/smashing-homepage.png" */}
